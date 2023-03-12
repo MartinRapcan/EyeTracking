93dcea4668b4ea2f95035c96b542993210079f49
MartinRapcan <rapcan.rmartin@gmail.com>
2023-03-12 23:52:28 +0100

    feat: getting uv coordinates on screen

--- pye3d_example.py
+++ pye3d_example.py
@@ -3,90 +3,62 @@ import argparse
 import cv2
 from pupil_detectors import Detector2D
 import numpy as np
+import sys
 import math
 from pye3d.detector_3d import CameraModel, Detector3D, DetectorMode
 
 # create 2D detector
 detector_2d = Detector2D()
+detector_2d_properties = {
+    "intensity_range":32,
+    "pupil_size_max": 120,
+    "pupil_size_min": 50,
+}
+detector_2d.update_properties(detector_2d_properties)
+
 # create pye3D detector
 camera = CameraModel(focal_length=772.55, resolution=[640, 480])
 detector_3d_original = Detector3D(camera=camera, long_term_mode=DetectorMode.blocking)
-detector_3d_test = Detector3D(camera=camera, 
-                              long_term_mode=DetectorMode.blocking,
-                              threshold_swirski=0.95,
-                              threshold_kalman=0.98,
-                              long_term_forget_time=20,
-                              long_term_buffer_size=50)
-# load eye video
-counter = 0
+
 rep = 0
 data = {}
 
-def loop(video):
-    global counter, data, rep
-    while video.isOpened():
-        frame_number = video.get(cv2.CAP_PROP_POS_FRAMES)
-        fps = video.get(cv2.CAP_PROP_FPS)
-        ret, eye_frame = video.read()
-        if ret:
-            # read video frame as numpy array
-            grayscale_array = cv2.cvtColor(eye_frame, cv2.COLOR_BGR2GRAY)
-            eye_frame_test = eye_frame.copy()
-            # run 2D detector on video frame
-            result_2d = detector_2d.detect(grayscale_array)
-            result_2d["timestamp"] = frame_number / fps
-            # pass 2D detection result to 3D detector
-            result_3d_original = detector_3d_original.update_and_detect(result_2d, grayscale_array)
-            result_3d_test = detector_3d_test.update_and_detect(result_2d, grayscale_array)
+def loop():
+    for i in range(121):
+        image = cv2.imread(f"dataset-Vincur/synthetizedImages_no_glint_denoised/example_{i}.png")
+        # read video frame as numpy array
+        grayscale_array = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # run 2D detector on video frame
+        result_2d = detector_2d.detect(grayscale_array)
+        result_2d["timestamp"] = i
+        # pass 2D detection result to 3D detector
+        result_3d_original = detector_3d_original.update_and_detect(result_2d, grayscale_array, apply_refraction_correction=False)
 
-            # TODO: uncomment for future data collection
-            if rep:
-                data[counter] = result_3d_original
-            ellipse_3d_original = result_3d_original["ellipse"]
-            ellipse_3d_test = result_3d_test["ellipse"]
-            # draw 3D detection result on eye frame
-            cv2.ellipse(
-                eye_frame,
-                tuple(int(v) for v in ellipse_3d_original["center"]),
-                tuple(int(v / 2) for v in ellipse_3d_original["axes"]),
-                ellipse_3d_test["angle"],
-                0,
-                360,  # start/end angle for drawing
-                (0, 255, 0),  # color (BGR): red
-            )
-            cv2.ellipse(
-                eye_frame_test,
-                tuple(int(v) for v in ellipse_3d_test["center"]),
-                tuple(int(v / 2) for v in ellipse_3d_test["axes"]),
-                ellipse_3d_test["angle"],
-                0,
-                360,  # start/end angle for drawing
-                (0, 255, 0),  # color (BGR): red
-            )
+        # TODO: uncomment for future data collection
+        if rep:
+            data[i] = result_3d_original
+        ellipse_3d_original = result_3d_original["ellipse"]
+        # draw 3D detection result on eye frame
+        cv2.ellipse(
+            image,
+            tuple(int(v) for v in ellipse_3d_original["center"]),
+            tuple(int(v / 2) for v in ellipse_3d_original["axes"]),
+            ellipse_3d_original["angle"],
+            0,
+            360,  # start/end angle for drawing
+            (0, 255, 0),  # color (BGR): red
+        )
 
-            # show frame
-            cv2.imshow("Detector3D-original", eye_frame)  
-            cv2.imshow("Detector3D-test", eye_frame_test)
-            #print("Frame number: ", counter)
-            cv2.waitKey(5)
-        
-        counter += 1
-        if counter == 120:
-            counter = 0
-            rep = 1
-            break
+        # show frame
+        cv2.imshow("Detector3D-original", image)  
+        cv2.waitKey(5)
 
 
 def main(eye_video_path):
-
-    
-    eye_video = cv2.VideoCapture(eye_video_path)
-    # read each frame of video and run pupil detectors
-    loop(eye_video)
-    eye_video.release()
-    eye_video = cv2.VideoCapture(eye_video_path)
-    loop(eye_video)
-    eye_video.release()
+    global rep
+    loop()
+    rep = 1
+    loop()
     cv2.destroyAllWindows()
 
 
@@ -99,16 +71,67 @@ EYE_TARGET = [None, -500, None]
 def dir_vector(vec1, vec2):
     return [vec2[0] - vec1[0], vec2[1] - vec1[1], vec2[2] - vec1[2]]
 
+def transfer_vector(vec):
+    return [round(vec[0], 2), round(vec[2] - 50, 2), round(-vec[1], 2)]
+
+def intersectPlane(n, p0, l0, l):
+    denom = matmul(-n, l)
+    if (denom > sys.float_info.min):
+        p0l0 = p0 - l0
+        t = matmul(p0l0, -n) / denom
+        return t
+    return -1.0
+    
+def matmul(v1, v2, pad=False, padBy=1.0):
+    if(pad is True):
+        return np.matmul(v1, np.append(v2, padBy))[:-1]
+    return np.matmul(v1, v2)
+    
+def getPoint(ray, distance):
+    return ray[0] + ray[1] * distance
+
+def normalize(v):
+    return v / magnitude(v)
+    
+def magnitude(v):
+    return np.sqrt(sqrMagnitude(v))
+        
+def sqrMagnitude(v):
+    return matmul(v, v)
+
+def convert_to_uv(vec, size_x=250, size_y=250, flip_y=True):
+    x = (vec[0] + size_x / 2) / size_x
+    if flip_y:
+        y = (-vec[2] + size_y / 2) / size_y
+    else:
+        y = (vec[2] + size_y / 2) / size_y
+    return (max(0, min(1, x)), max(0, min(1, y)))
+
+planeNormal = np.array([0, 1, 0])
+planeCenter = np.array([0, -500, 0])
 dir_vectors = {}
+uv_coords = []
 def calcPoint():
     for i in data:
-        dir_vectors[i] = dir_vector(data[i]['sphere']['center'], data[i]['circle_3d']['center'])
+        dir_vectors[i] = {"sphere": np.array(transfer_vector(data[i]["sphere"]["center"])),
+                          "circle_3d": np.array(transfer_vector(data[i]["circle_3d"]["center"]))}
 
     for i in dir_vectors:
-        print(dir_vectors[i])
+        rayOrigin = dir_vectors[i]["sphere"]
+        rayDirection = normalize(np.array(dir_vectors[i]["circle_3d"]) - dir_vectors[i]["sphere"])
+        intersectionTime = intersectPlane(planeNormal, planeCenter, rayOrigin, rayDirection)
+        if (intersectionTime > 0.0):
+            planeIntersection = getPoint([rayOrigin, rayDirection], intersectionTime)
+            planeIntersection[0] = -planeIntersection[0]
+            uv_coords.append(convert_to_uv(planeIntersection))
+
+# save it as csv x, y
+def saveUVCoords():
+    with open('coordinates/uv_coords.csv', 'w') as f:
+        for uv in uv_coords:
+            f.write(f'{uv[0]}, {uv[1]}\n')
 
 if __name__ == "__main__":
-    #main('dataset-Vincur/test/example_0.png')
-    #TODO: images that not being detected correctly 18 43 71 86 87 108
     main('dataset-Vincur/synthetizedImages_no_glint_denoised/example_0.png')
     calcPoint()
+    saveUVCoords()
---
+++ scanpath.py
@@ -0,0 +1,139 @@
+import csv
+import cv2
+from random import randint, sample
+from math import sqrt, atan2, cos, sin
+
+input_path = "./coordinates/uv_coords.csv"
+image_path = "./images/scan_test.jpg"
+
+image = cv2.imread(image_path)
+image_width = image.shape[1]
+image_height = image.shape[0]
+
+def convert_uv_to_px(uv_data, width, height):
+    return (int(uv_data[0] * width), int(uv_data[1] * height))
+
+overlay_circles = image.copy()
+overlay_lines = image.copy()
+alpha_circles = 0.6
+alpha_lines = 0.2
+TEXT_FACE = cv2.FONT_HERSHEY_SIMPLEX
+TEXT_SCALE = 0.8
+TEXT_THICKNESS = 2
+TEXT_COLOR = (0, 0, 0)
+
+points_group = {}
+colors = {}
+threshold = 150
+order = 0
+
+with open(input_path) as f:
+	reader = csv.reader(f)
+	raw = list(map(lambda q: (float(q[0]), float(q[1])), reader))
+
+for i in range(0, len(raw)):
+    raw[i] = convert_uv_to_px(raw[i], image_width, image_height)
+
+main_point = None
+for i in range(0, len(raw)):
+    if not main_point:
+        main_point = (raw[i][0], raw[i][1])
+
+    if abs(raw[i][0] - main_point[0]) <= threshold and abs(raw[i][1] - main_point[1]) <= threshold:
+        if not points_group.get(order):
+            points_group[order] = {'points': [raw[i]], 'middle': {'x': 0, 'y': 0}, 'diameter': 0, 'index': order + 1}
+        else:
+            points_group[order]['points'].append(raw[i])
+    
+    else:
+        order += 1
+        main_point = (raw[i][0], raw[i][1])
+        points_group[order] = {'points': [raw[i]], 'middle': {'x': 0, 'y': 0}, 'diameter': 0, 'index': order + 1}
+
+points_group = dict(sorted(points_group.items(), key=lambda item: len(item[1]['points']), reverse=False))
+points_group_keys = list(points_group)    
+
+for key in points_group:
+    points = points_group[key]['points']
+    x = 0
+    y = 0
+    diameter = 0
+
+    for point in points:
+        x += point[0]
+        y += point[1]
+
+    x = int(x / len(points))
+    y = int(y / len(points))
+    points_group[key]['middle']['x'] = x
+    points_group[key]['middle']['y'] = y
+    points_group[key]['diameter'] = 20
+    
+    
+# TODO: lepši sposob ako spraviť velkosť kruhu na zaklade počtu bodov v skupine
+# TODO: preto pokial mam jeden tak ono to nejako dojebe
+# q1_index = int(len(points_group) * 0.25)
+# q2_index = int(len(points_group) * 0.5)
+# q3_index = int(len(points_group) * 0.75)
+
+# q1 = points_group_keys[:q1_index]
+# q2 = points_group_keys[q1_index:q2_index]
+# q3 = points_group_keys[q2_index:q3_index]
+# q4 = points_group_keys[q3_index:]
+
+# def setDiameter(array, diameter):
+#     for key in array:
+#         points_group[key]['diameter'] = diameter
+        
+
+# setDiameter(q1, 30)
+# setDiameter(q2, 50)
+# setDiameter(q3, 70)
+# setDiameter(q4, 100)
+
+points_group = dict(sorted(points_group.items(), key=lambda item: item[1]['index'], reverse=False))
+points_group_keys = list(points_group)
+
+color_step = 1 / len(points_group)
+for key in points_group:
+    shade = key * color_step
+    colors[key] = (int(255 * shade), int(255 * shade), int(255 * shade))
+
+for key in range(0, len(points_group) - 1):
+    x1 = points_group[points_group_keys[key]]['middle']['x']
+    y1 = points_group[points_group_keys[key]]['middle']['y']
+    x2 = points_group[points_group_keys[key + 1]]['middle']['x']
+    y2 = points_group[points_group_keys[key + 1]]['middle']['y']
+    radius1 = points_group[points_group_keys[key]]['diameter']
+    radius2 = points_group[points_group_keys[key + 1]]['diameter']
+
+    distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    # TODO: pridať k riadiusu širku outline kruhu .. lebo to ide cez neho a vidieť to
+    if distance >= (radius1 + radius2):   
+        angle = atan2(y2 - y1, x2 - x1)
+        point1_x = x1 + radius1 * cos(angle)
+        point1_y = y1 + radius1 * sin(angle)
+        point2_x = x2 - radius2 * cos(angle)
+        point2_y = y2 - radius2 * sin(angle)
+        cv2.line(overlay_lines, (int(point1_x), int(point1_y)), (int(point2_x), int(point2_y)), colors[list(points_group)[key]], 4)
+    
+    text_size, _ = cv2.getTextSize(str(points_group[points_group_keys[key]]['index']), TEXT_FACE, TEXT_SCALE, TEXT_THICKNESS)
+    text_origin = (int(x1 - text_size[0] / 2), int(y1 + text_size[1] / 2))
+    
+    #cv2.circle(overlay_circles, (x1, y1), radius1, colors[points_group_keys[key]], -1)
+    cv2.circle(overlay_circles, (x1, y1), radius1, colors[points_group_keys[key]], 10)
+    #cv2.putText(overlay_circles, str(points_group[points_group_keys[key]]['index']), text_origin, TEXT_FACE, TEXT_SCALE, TEXT_COLOR, TEXT_THICKNESS, cv2.LINE_AA)
+    if key == len(points_group) - 2:
+        text_size, _ = cv2.getTextSize(str(points_group[points_group_keys[key]]['index']), TEXT_FACE, TEXT_SCALE, TEXT_THICKNESS)
+        text_origin = (int(x2 - text_size[0] / 2), int(y2 + text_size[1] / 2))
+        #cv2.circle(overlay_circles, (x2, y2), radius2, colors[points_group_keys[key + 1]], -1)
+        cv2.circle(overlay_circles, (x2, y2), radius2, colors[points_group_keys[key + 1]], 10)
+        #cv2.putText(overlay_circles, str(points_group[points_group_keys[key + 1]]['index']), text_origin, TEXT_FACE, TEXT_SCALE, TEXT_COLOR, TEXT_THICKNESS, cv2.LINE_AA)
+
+
+result = cv2.addWeighted(overlay_circles, alpha_circles, image, 1 - alpha_circles, 0)
+result = cv2.addWeighted(overlay_lines, alpha_lines, result, 1 - alpha_lines, 0)
+
+# TODO: generate multiple images with different types of scanpath
+cv2.imwrite("./images/scan_test_after.jpg", result)
